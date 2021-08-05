406b705a6566d88ebd5b0aaa332e4c1507d62cd5
Damian-MG <franciscodamia.maleno@estudiants.urv.cat>
2021-08-05 15:12:21 +0200

    test added

---
+++ test_functions.py
@@ -0,0 +1,28 @@
+from lithops import Storage
+import subprocess as sp
+import os.path
+import lithopsgenetics
+
+BUCKET_NAME = 'damianbucket'  # change-me, REMEMBER TO DELETE FILES FROM BUCKET WHEN REPROCESSING
+BUCKET_LINK = 'https://damianbucket.s3.eu-de.cloud-object-storage.appdomain.cloud/' # change-me
+
+# This program takes a gzfile stored locally in the machine generates an index and information files for it,
+# uploads them to an IBM Cloud Storage bucket to perform the partioning of the gzfile
+
+# MAIN PROGRAM
+if __name__ == '__main__':
+
+    # FUNCTION preprocess_chunk_complete_file(BUCKET_NAME, BUCKET_LINK, file, lines)
+    lithopsgenetics.preprocess_chunk_complete_gzfile(BUCKET_NAME, BUCKET_LINK, '1c-12S_S96_L001_R1_001.fastq.gz', '100000')
+
+    # FUNCTION retrieve_random_chunk_gzfile(BUCKET_NAME, BUCKET_LINK, file, start_line, end_line)
+    #lithopsgenetics.retrieve_random_chunk_gzfile(BUCKET_NAME, BUCKET_LINK, '1c-12S_S96_L001_R1_001.fastq.gz', '101000', '150000')
+    
+    # FUNCTION preprocess_gzfile(BUCKET_NAME, file)
+    #lithopsgenetics.preprocess_gzfile(BUCKET_NAME, '1c-12S_S96_L001_R1_001.fastq.gz')
+
+    # FUNCTION chunk_complete_gzfile(BUCKET_NAME, BUCKET_LINK, file, lines, total_lines)
+    #lithopsgenetics.chunk_complete_gzfile(BUCKET_NAME, BUCKET_LINK, '1c-12S_S96_L001_R1_001.fastq.gz', '100000', '309956')
+
+
+       
